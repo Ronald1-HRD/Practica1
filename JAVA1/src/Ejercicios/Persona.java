7d7d3b8559f27e1fea050bf456f2881e16d9fb92
Ronald1-HRD <146364485+Ronald1-HRD@users.noreply.github.com>
2023-10-05 11:13:11 -0400

    Persona.java

--- JAVA1/src/Ejercicios/Persona.java
+++ JAVA1/src/Ejercicios/Persona.java
@@ -1,17 +1,20 @@
 package Ejercicios;
 
 public class Persona {
+    //Atributos
     private String nombre;
     private String apellido;
     private int edad;
 
     Persona(){}
+    //Constructores
 
     public Persona(String nombre,String apellido,int edad){
         this.nombre=nombre;
         this.apellido=apellido;
         this.edad=edad;
     }
+    //Metodos y comportamientos
 
     public void imprimirDatos(){
         System.out.println("Nombre de la persona:"+nombre);
